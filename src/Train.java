78faf4c2a2ed455508754cd5ced6e979d464f918
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-09-13 15:24:31 +0700

    +

--- src/Train.java
+++ src/Train.java
@@ -3,7 +3,7 @@ public class Train extends Vehicle {
 
 	@Override
 	public void drive() {
-		System.out.print("Sit train to go to London!");
+		System.out.print("I will sit the train to go to London!");
 
 	}
 
